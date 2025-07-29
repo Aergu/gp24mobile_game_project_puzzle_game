eb87a65c7276a77048fdffd1b3c47d2f48641e1e
Joshua Aroka <josaro8260@gmail.com>
2025-07-29 14:56:38 +0200

    Made some progress on the script

---
+++ Memory_Game/Assets/Scripts/CardController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardController : MonoBehaviour
+{
+ [SerializeField] private Card cardPrefab;
+ [SerializeField] private Transform gridTransform;
+ [SerializeField] private Sprite[] sprites;
+
+ private List<Sprite> spritePairs;
+
+ void start()
+ {
+  PrepareSprites();
+  CreateCards();
+ }
+
+ private void PrepareSprites()
+ {
+  spritePairs = new List<Sprite>();
+  for (int i = 0; i < sprites.Length; i++)
+  {
+   spritePairs.Add(sprites[i]);
+   spritePairs.Add(sprites[i]);
+  }
+
+  ShuffleSprites(spritePairs);
+ }
+
+ void CreateCards()
+ {
+  for (int i = 0; i < spritePairs.Count; i++)
+  {
+   Card card = Instantiate(cardPrefab, gridTransform);
+   card.SetIconSprite(spritePairs[i]);
+   
+  }
+ }
+
+ void ShuffleSprites(List<Sprite> spriteList)
+ {
+  for (int i = spriteList.Count - 1; i > 0; i--)
+  {
+   int randomIndex = Random.Range(0, i + 1);
+
+   Sprite temp = spriteList[i];
+   spriteList[i] = spriteList[randomIndex];
+   spriteList[randomIndex] = temp;
+  }
+ }
+}
